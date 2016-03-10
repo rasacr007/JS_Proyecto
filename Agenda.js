5cd1a1f4778e83d5a9787b0684bf0ee1f42c4dd5
Randall Sanabria <sanabria7@hotmail.com>
2016-03-10 17:00:52 -0600

    Update Agenda.js

--- Agenda.js
+++ Agenda.js
@@ -1,17 +1,54 @@
+
 var Agenda = function() {
     var ContactsArray =[];
 
-  return {
-    listContacts : function (){
+    function addContacts(name, lastName, email,phone){
+        ContactsArray.push({Name:name,LastName:lastName, Email:email, Phone:phone});
+    };
+
+    function updateContact(name, lastName, email,phone){
+       var pos = ContactsArray.indexOf(name);
+          ContactsArray[pos].Name=name;
+         ContactsArray[pos].LastName=lastName;
+         ContactsArray[pos].Email=email;
+         ContactsArray[pos].Phone=phone;
+       };
+
+
+    /*   with (ContactsArray[pos]) {
+         Name=name;
+         LastName=lastName;
+         Email=email;
+         Phone=phone;
+       };*/
+    };
+
+    function deleteContact(name){
+      var pos = ContactsArray.indexOf(name);
+      ContactsArray.splice(pos,1);
+     };
+
+    function listContacts(){
+      return ContactsArray;
       /*ContactsArray.forEach(function(reg, indice, Arreglo){
         console.log(reg.Name);
       }
     );*/
-    return ContactsArray;
+    };
 
+
+  return {
+    listarContactos : function (){
+    return listContacts();
     },
-    addContacts : function (name, lastName, email,phone){
-      ContactsArray.push({Name:name,LastName:lastName, Email:email, Phone:phone});
+    crearContacto: function (name, lastName, email,phone){
+      addContacts(name,lastName, email,phone);
+    },
+     editarContacto(name,lastName, email,phone){
+       updateContact(name,lastName, email,phone);
+     },
+     borrarContacto(name){
+       deleteContact(name);
      }
   };
 };
