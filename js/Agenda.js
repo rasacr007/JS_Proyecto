f2208517280d1b790aa4a3dd3b9e775501e2bbdd
Randall Sanabria <sanabria7@hotmail.com>
2016-04-10 17:26:35 -0600

    10 abril
    Cambio al 10

--- js/Agenda.js
+++ js/Agenda.js
@@ -1,7 +1,9 @@
 
+
 var Agenda = function() {
     var ContactsArray =[];
     var message_recNotFound ='Registro no encontrado'
+
 
     function addContacts(name, lastName, email,phone){
         var pos= findMaxPosition();
@@ -25,16 +27,16 @@ var Agenda = function() {
         return result;
     };
 
-    function updateContact(pos,name, lastName, email,phone){
-       var postmp = findPos(pos);
+    function updateContact(par_pos,par_name, par_lastName,par_email,par_phone){
+       var postmp = findPos(par_pos);
        if (postmp<0){
          throw new Error(message_recNotFound);
        };
        with (ContactsArray[postmp]) {
-         name=name;
-         lastName=lastName;
-         email=email;
-         phone=phone;
+         name=par_name;
+         lastName=par_lastName;
+         email=par_email;
+         phone=par_phone;
        };
 
     };
@@ -76,13 +78,121 @@ var Agenda = function() {
      borrarContacto(position){
         deleteContact(position);
      },
-
      cargarDatosPrueba(){
-         addContacts("Randall","Sanabria","sanabria7@hotmail.com","506-8801-9698");
-         addContacts("Juan","Perez","perez@hotmail.com","505-8801-9698");
-         addContacts("Carlos","Mena","mena@hotmail.com","507-8801-9698");
-         addContacts("Raul","Mora","mora@hotmail.com","508-8801-9698");
+
+           addContacts("Randall","Sanabria","sanabria7@hotmail.com","8801-9698");
+           addContacts("Juan","Perez","perez@hotmail.com","8801-9698");
+           addContacts("Carlos","Mena","mena@hotmail.com","8801-9698");
+           addContacts("Raul","Mora","mora@hotmail.com","8801-9698");
+
+
      }
+
+
 
   };
 };
+
+
+
+
+var objAgenda= new Agenda();
+function cargarPantallaListar()
+{
+      var Registros=[];
+      var x ="<thead><tr><th>Nombre</th><th>Apellido</th><th>Telefono</th><th>Email</th><th></th><th></th></tr></thead>"
+      x+="<tbody>";
+      //var regAgenda = new Agenda();
+      objAgenda.cargarDatosPrueba();
+      Registros= objAgenda.listarContactos();
+       Registros.forEach( function (item){
+            x+="<tr>" +
+            "<td>"+item.name+"</td>"+
+            "<td>"+item.lastName+"</td>"+
+            "<td>"+item.phone+"</td>"+
+            "<td>"+item.email+"</td>"+
+            "<td>"+"<a href=Edit.html?accion=2&position=" + item.position+">"
+            +"<img src=edit-24.ico alt=Editar title=Editar>"+"</a></td>"+
+            "<td>"+
+            "<a href=Edit.html?accion=3&position=" + item.position+">"
+            +"<img src=delete-24.ico alt=Borrar title=Borrar>"+"</a></td>"+
+            "</tr>";
+      });
+      x+="</tbody>";
+      document.getElementById("tabla").innerHTML = x;
+}
+
+
+function cargarPantallaEditar(){
+  objAgenda.cargarDatosPrueba()
+  document.querySelector('#cancelar').addEventListener('click', volverPagina);
+  document.querySelector('#guardar').addEventListener('click', guardar);
+  var accionPantalla=parseInt(getParameterByName("accion",  window.location.href))
+  if (accionPantalla!==1)
+    cargarDatos();
+
+
+
+  var tituloPantalla
+   switch (accionPantalla){
+                          case 1:
+                            tituloPantalla='Crear Contacto';
+                                  break;
+                          case 2:
+                          tituloPantalla= 'Editar Contacto';
+                                  break;
+                          default :
+                            tituloPantalla='Borrar Contacto';
+                            break;
+                          };
+
+document.querySelector('h2').innerHTML=tituloPantalla;
+}
+function volverPagina(){
+    javascript:history.back(-1);
+};
+
+function cargarDatos(){
+
+  var param = getParameterByName("position",  window.location.href);
+  //var objAgenda = new Agenda();
+  var obj= objAgenda.listarContacto(param);
+      document.getElementById("nombre").value=obj.name;
+      document.getElementById("apellido").value=obj.lastName;
+      document.getElementById("email").value=obj.email;
+      document.getElementById("telefono").value=obj.phone;
+ }
+
+ function guardar(){
+   var accionPantalla=parseInt(getParameterByName("accion",  window.location.href));
+   //var objAgenda = new Agenda();
+
+   if (accionPantalla ===1){
+      objAgenda.crearContacto(document.getElementById("nombre").value,
+      document.getElementById("apellido").value,
+      document.getElementById("email").value,
+      document.getElementById("telefono").value);
+   }
+   else if (accionPantalla ===2){
+     objAgenda.editarContacto(
+     getParameterByName("position",  window.location.href),
+     document.getElementById("nombre").value,
+     document.getElementById("apellido").value,
+     document.getElementById("email").value,
+     document.getElementById("telefono").value);
+   }
+   else {
+     objAgenda.borrarContacto(
+     getParameterByName("position",  window.location.href))
+   }
+}
+
+ function getParameterByName(name, url) {
+     if (!url) url = window.location.href;
+     name = name.replace(/[\[\]]/g, "\\$&");
+     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", "i"),
+         results = regex.exec(url);
+     if (!results) return null;
+     if (!results[2]) return '';
+     return decodeURIComponent(results[2].replace(/\+/g, " "));
+ }
